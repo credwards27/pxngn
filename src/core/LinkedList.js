d4ca84ee58dd09dd3a9e365cc896711df521334f
credwards27 <credwards27@users.noreply.github.com>
2016-11-08 21:54:55 -0500

    Added empty class for LinkedList structure

---
+++ src/core/LinkedList.js
@@ -0,0 +1,33 @@
+/* LinkedList.js
+    
+    Doubly linked, reversible list data structure.
+*/
+
+/* Doubly linked list data structure.
+*/
+export default class LinkedList {
+    //
+    // STATIC PROPERTIES
+    //
+    
+    //
+    // PROPERTIES
+    //
+    
+    /* Constructor for LinkedList.
+    */
+    constructor() {
+    }
+    
+    /* Destructor for LinkedList.
+    */
+    destroy() {}
+    
+    //
+    // STATIC METHODS
+    //
+    
+    //
+    // METHODS
+    //
+}
