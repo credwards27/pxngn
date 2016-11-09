2533c5649dba1d1e11d9b6a562143cae572f5d82
credwards27 <credwards27@users.noreply.github.com>
2016-11-08 23:10:13 -0500

    Added head, tail, and next/prev pointer properties to LinkedList

--- src/core/LinkedList.js
+++ src/core/LinkedList.js
@@ -14,6 +14,24 @@ export default class LinkedList {
     // PROPERTIES
     //
     
+    // Head node.
+    _head = {
+        0: null,
+        1: null
+    };
+    
+    // Tail node.
+    _tail = {
+        0: null,
+        1: null
+    };
+    
+    // Next node index.
+    _next = 1;
+    
+    // Previous node index.
+    _prev = 0;
+    
     /* Constructor for LinkedList.
     */
     constructor() {
