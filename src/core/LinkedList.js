4c5cd7198b6cf6e1fd22aaf7525e3c23b1bed42a
credwards27 <credwards27@users.noreply.github.com>
2016-11-11 21:39:27 -0500

    Added empty method definitions with initial documentation to LinkedList

--- src/core/LinkedList.js
+++ src/core/LinkedList.js
@@ -33,8 +33,10 @@ export default class LinkedList {
     _prev = 0;
     
     /* Constructor for LinkedList.
+        
+        data - Item or array of items to be added on instantiation.
     */
-    constructor() {
+    constructor(data) {
     }
     
     /* Destructor for LinkedList.
@@ -48,4 +50,150 @@ export default class LinkedList {
     //
     // METHODS
     //
+    
+    /* Appends an item to the end of the list.
+        
+        data - Data to append to the list.
+        
+        Returns the new node, or null if no node was added.
+    */
+    append(data) {
+    }
+    
+    /* Prepends an item to the beginning of the list.
+        
+        data - Data to prepend to the list.
+        
+        Returns the new node, or null if no node was added.
+    */
+    prepend(data) {
+    }
+    
+    /* Inserts data after a given node.
+        
+        data - Data to insert.
+        after - Existing node after which to insert data.
+        
+        Returns the new node, or null if no node was added.
+    */
+    insertAfter(data, after) {
+    }
+    
+    /* Inserts data before a given node.
+        
+        data - Data to insert.
+        before - Existing node before which to insert data.
+        
+        Returns the new node, or null if no node was added.
+    */
+    insertBefore(data, before) {
+    }
+    
+    /* Removes the last node from the list and returns its data. Similar to
+        Array.prototype.pop(), the last node is removed.
+        
+        Returns the removed node's data, or null if the list was empty when
+        called. NOTE: This will also return null if the last node in the list's
+        data is null.
+    */
+    pop() {
+    }
+    
+    /* Removes the first node from the list and returns its data. Similar to
+        Array.prototype.shift(), the first node is removed.
+        
+        Returns the removed node's data, or null if the list was empty when
+        called. NOTE: This will also return null if the first in the list's data
+        is null.
+    */
+    shift() {
+    }
+    
+    /* Removes a node from the list and destroys it.
+        
+        node - Node to be removed.
+        
+        Returns true if the node was removed, or false if the node was not
+        removed.
+    */
+    removeNode(node) {
+    }
+    
+    /* Gets the first node in the list.
+        
+        Returns the first node, or null if the list is empty.
+    */
+    getFirst() {
+    }
+    
+    /* Gets the last node in the list.
+        
+        Returns the last node, or null if the list is empty.
+    */
+    getLast() {
+    }
+    
+    /* Iterates through each node in the list.
+        
+        callback - Callback function to run on each iteration. Expects the
+            following parameters:
+                data - Data contained by the node at the current iteration.
+                node - Node at the current iteration.
+            
+            Returning explicitly false from the callback will stop iteration.
+            In addition to the node parameter, 'this' in the callback function
+            refers to the node at the current iteration.
+            
+            Callbacks may remove nodes with removeNode() without affecting
+            iteration, though doing so may cause problems if other iterations
+            are running asynchronously.
+        
+        thisArg - Optional object to be used for 'this' in the callback instead
+            of the current node.
+        
+        Returns the list.
+    */
+    each(callback, thisArg) {
+    }
+    
+    /* Iterates through the list in reverse.
+        
+        callback - See LinkedList::each().
+        thisArg - See LinkedList::each().
+        
+        Returns the list.
+    */
+    eachReverse(callback, thisArg) {
+    }
+    
+    /* Reverses the list.
+        
+        Returns the list.
+    */
+    reverse() {
+    }
+    
+    /* Checks if the list is reversed.
+        
+        Returns true if the list is reversed, false otherwise.
+    */
+    isReversed() {
+    }
+    
+    /* Iterates through the list with an arbitrary starting point, ending point,
+        and direction.
+        
+        callback - See LinkedList::each().
+        thisArg - See LinkedList::each().
+        head - Head node for iteration. The first iteration will be the node
+            after this one.
+        tail - Tail node for iteration. The last iteration will be the node
+            before this one.
+        nextIndex - Index to use for the 'next' direction (0 or 1). The
+            'previous' direction will be the opposite of this.
+        
+        Returns the list.
+    */
+    _iterate(callback, thisArg, head, tail, nextIndex) {
+    }
 }
